cd06de3e0a25b72902603a97fdf3d74cc277712a
nathanjsoares <nathansoares@gmail.com>
2026-02-18 18:30:11 -0300

    post put delete and DatabaseException

--- src/main/java/com/devsuperio/apiCliente/services/ClientService.java
+++ src/main/java/com/devsuperio/apiCliente/services/ClientService.java
@@ -3,14 +3,16 @@ package com.devsuperio.apiCliente.services;
 import com.devsuperio.apiCliente.dto.ClientDTO;
 import com.devsuperio.apiCliente.entities.Client;
 import com.devsuperio.apiCliente.repositories.ClientRepository;
+import com.devsuperio.apiCliente.services.exceptions.DatabaseException;
 import com.devsuperio.apiCliente.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class ClientService {
@@ -28,5 +30,46 @@ public class ClientService {
     public Page<ClientDTO> findAll(Pageable pageable){
         Page<Client> result = repository.findAll(pageable);
         return  result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto){
+        Client entity = new Client();
+        copyDTOtoEntity(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update(Long id,ClientDTO dto){
+        try{
+            Client entity = repository.getReferenceById(id);
+            copyDTOtoEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ClientDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id){
+        if (!repository.existsById(id)){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try{
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDTOtoEntity(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 }
---
+++ src/main/java/com/devsuperio/apiCliente/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.devsuperio.apiCliente.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String msg){
+        super(msg);
+    }
+}
